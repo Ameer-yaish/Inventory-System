fbe61c04fcda2b3c3771de366bcd8fb6e0477188
ameer yaesh <ameer1momo1@gmail.com>
2022-12-26 15:48:46 +0200

    first commit

---
+++ inventory system/src/inventory/system/InventorySystem.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventory.system;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author seazer
+ */
+public class InventorySystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+       Login a=new Login();
+       a.setVisible(true);a.setLocationRelativeTo(null);
+a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+// TODO code application logic here
+    }
+    
+}
